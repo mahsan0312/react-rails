29c6ae6210fa845c38a73c3d8bd7fde23387ed85
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-10-22 23:42:38 +0500

    Improvements

--- app/javascript/react/src/components/Hello.jsx
+++ app/javascript/react/src/components/Hello.jsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
 const Hello = () => {
-  return (<div>Hello, Rails 7!</div>)
+  return (<div>Hello, My name is Muhammad Ahsan</div>)
 }
 
 // Use it if you don't plan to use "remount"
